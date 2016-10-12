d7dbbdc5d2ae2611b9d27e9b8de360858acd6f30
Trenton Kress <trenton.kress@gmail.com>
2016-10-11 18:40:01 -0700

    auth  system started. added a token authenticator with jwt

--- server.js
+++ server.js
@@ -69,8 +69,9 @@ app.use(bodyParser.urlencoded(
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
+  res.setHeader('Access-Control-Allow-Origin', 'http://trentonkress.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET POST PUT');
   next();
 });
 
@@ -78,24 +79,79 @@ app.get('/', function(req, res) {
   res.end('Welcome to the empty root. There\'s nothing to see here!');
 });
 
-app.post('/login', function(req, res) {
-  Auth.findOne({ username: req.body.details.username }, function(err, user) {
+app.get('/projects', function(req, res) {
+  Project.find(function(err, projects) {
+    if (err)
+      console.log(err);
+    res.json(projects);
+  });
+});
+
+app.get('/projects/:id', function(req, res) {
+  Project.findById(req.params.id, function(err, project) {
+    if (err)
+      console.log(err);
+    res.json(project);
+  });
+});
+
+const apiRoutes = express.Router();
+
+apiRoutes.post('/login', function(req, res) {
+  Auth.findOne({ username: req.body.details.username }, function(err, auth) {
     if (err)
       throw err;
-    user.checkPass(req.body.details.password, function(err, isMatch) {
+    auth.checkPass(req.body.details.password, function(err, isMatch) {
       if (err)
         throw err;
       console.log('Authentication request for ' + req.body.details.username, isMatch);
       if (isMatch) {
-        res.end('Logged in.');
-      } else {
-        res.end('Invalid login details.');
+        let token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Login sucessful.',
+          token: token
+        });
       }
     });
   });
 });
 
-app.post('/projects', function(req, res) {
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+  }
+});
+
+apiRoutes.post('/projects', function(req, res) {
   let projectParams = {
     name:'Default Filler',
     role:'Programmer',
@@ -128,23 +184,7 @@ app.post('/projects', function(req, res) {
   });
 });
 
-app.get('/projects', function(req, res) {
-  Project.find(function(err, projects) {
-    if (err)
-      console.log(err);
-    res.json(projects);
-  });
-});
-
-app.get('/projects/:id', function(req, res) {
-  Project.findById(req.params.id, function(err, project) {
-    if (err)
-      console.log(err);
-    res.json(project);
-  });
-});
-
-app.put('/projects/:id', function(req, res, next) {
+apiRoutes.put('/projects/:id', function(req, res) {
   Project.findByIdAndUpdate(req.params.id, req.body, function(err, project) {
     if (err)
       console.log(err);
@@ -152,7 +192,12 @@ app.put('/projects/:id', function(req, res, next) {
   });
 });
 
-app.get('/rs/items/:itemId', function(req, res) {
+app.use('/api', apiRoutes);
+
+const rsRoutes = express.Router();
+
+//"just for fun" mirror to test bypassing cross-origin limit BS
+rsRoutes.get('/rs/items/:itemId', function(req, res) {
   http.get('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=' + req.params.itemId, function(httpRes) {
     let body = '';
 
@@ -167,6 +212,8 @@ app.get('/rs/items/:itemId', function(req, res) {
     console.log('Error getting item details', e);
   });
 });
+
+app.use('/rs', apiRoutes);
 
 const server = app.listen(80, function() {
   console.log('Portfolio server listening at http://' + server.address().address + ':' + server.address().port);
