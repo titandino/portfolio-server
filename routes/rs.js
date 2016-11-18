a43c709ec55ad5ea8bb0e925beda5695e647d455
Trenton Kress <trenton.kress@gmail.com>
2016-11-17 18:58:02 -0800

    Finished organizing the RS endpoints to format the data to a much more usable format.

--- routes/rs.js
+++ routes/rs.js
@@ -5,6 +5,9 @@ const http = require('http');
 
 const router = express.Router();
 
+const SKILL_NAMES = ['Overall', 'Attack', 'Defence', 'Strength', 'Constitution', 'Ranged', 'Prayer', 'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility',
+  'Thieving', 'Slayer', 'Farming', 'Runecrafting', 'Hunter', 'Construction', 'Summoning', 'Dungeoneering', 'Divination', 'Invention']
+
 router.get('/items/:itemId', function(req, res) {
   http.get('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=' + req.params.itemId, function(httpRes) {
     let body = '';
@@ -14,7 +17,14 @@ router.get('/items/:itemId', function(req, res) {
     });
 
     httpRes.on('end', function() {
-      res.end(body);
+      let allItem = JSON.parse(body);
+      let item = {
+        name: allItem.item.name,
+        description: allItem.item.description,
+        image: allItem.item.icon_large,
+        price: allItem.item.current.price
+      };
+      res.json(item);
     });
   }).on('error', function(e) {
     console.log('Error getting item details', e);
@@ -30,8 +40,24 @@ router.get('/highscores/:username', function(req, res) {
     });
 
     httpRes.on('end', function() {
-      let statBlocks = body.split('\n');
-      res.json(statBlocks);
+      if (body.includes('DOCTYPE')) {
+        res.end('No stats found for player.');
+      } else {
+        let statBlocks = body.split('\n');
+        let stats = [];
+        for (let i = 0;i < statBlocks.length;i++) {
+          if (i >= SKILL_NAMES.length)
+            break;
+          let statRanks = statBlocks[i].split(',');
+          stats[i] = {
+            skillName: SKILL_NAMES[i],
+            rank: statRanks[0],
+            level: statRanks[1],
+            xp: statRanks[2]
+          };
+        }
+        res.json(stats);
+      }
     });
   }).on('error', function(e) {
     console.log('Error getting item details', e);
