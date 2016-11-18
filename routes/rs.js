335698d984b7589f184adbc0c064a81dddc6d883
Trenton Kress <trenton.kress@gmail.com>
2016-11-17 18:19:34 -0800

    Changed the rs routes again since the request proxy wasn't working.

--- routes/rs.js
+++ routes/rs.js
@@ -1,16 +1,40 @@
 'use strict';
 
 const express = require('express');
-const requestProxy = require('express-request-proxy');
+const http = require('http');
 
 const router = express.Router();
 
-router.get('/items/*', requestProxy({
-  url: 'http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=*'
-}));
+router.get('/items/:itemId', function(req, res) {
+  http.get('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=' + req.params.itemId, function(httpRes) {
+    let body = '';
 
-router.get('/highscores/*', requestProxy({
-  url: 'http://services.runescape.com/m=hiscore/index_lite.ws?player=*'
-}));
+    httpRes.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    httpRes.on('end', function() {
+      res.end(body);
+    });
+  }).on('error', function(e) {
+    console.log('Error getting item details', e);
+  });
+});
+
+router.get('/highscores/:username', function(req, res) {
+  http.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + req.params.username, function(httpRes) {
+    let body = '';
+
+    httpRes.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    httpRes.on('end', function() {
+      res.end(body);
+    });
+  }).on('error', function(e) {
+    console.log('Error getting item details', e);
+  });
+});
 
 module.exports = router;
